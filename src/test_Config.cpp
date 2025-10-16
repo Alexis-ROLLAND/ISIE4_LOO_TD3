1c384966530253028bbee8e489e6871a328faa36
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-10-16 14:15:42 +0200

    2025/2026 version OK

--- src/test_Config.cpp
+++ src/test_Config.cpp
@@ -1,51 +1,47 @@
+#include "Config.hpp"
 #include <iostream>
 #include <print>
-#include "Config.hpp"
 
-int main(){
-    std::println ("-- Test classe Config --");  /**< Affichage */
-    
-    Config myConfig{};  /**< New Config Object */
+int main() {
+    std::println("-- Test classe Config --"); /**< Affichage */
 
-    Config::pic24_register  TRISA{"TRISA",0xff00};
+    Config myConfig{}; /**< New Config Object */
+
+    Config::pic24_register TRISA{"TRISA", 0xff00};
 
     myConfig.add(TRISA);
-    myConfig.dump(); 
+    myConfig.dump();
 
-    try{
+    try {
         myConfig.add(TRISA);
-    }
-    catch(const std::exception &e){
-        std::println("Trying to add an existing register - Exception catched : {}",e.what());
+    } catch (const std::exception &e) {
+        std::println("Trying to add an existing register - Exception catched : {}", e.what());
     }
     myConfig.dump();
-    std::println("Size = {0:d}",myConfig.getSize());
+    std::println("Size = {0:d}", myConfig.getSize());
 
     TRISA.second = 0xAA55;
-    myConfig.add(TRISA,true);
+    myConfig.add(TRISA, true);
     myConfig.dump();
 
-    try{
+    try {
         myConfig.remove("LATA");
-    }
-    catch(const std::exception &e){
-        std::println("Trying to remove a non existing register - Exception catched : {}",e.what());
+    } catch (const std::exception &e) {
+        std::println("Trying to remove a non existing register - Exception catched : {}", e.what());
     }
     myConfig.dump();
 
     myConfig.remove(TRISA);
     if (myConfig.isEmpty()) std::println("Config is now empty - OK");
 
-
     myConfig.add(TRISA);
     myConfig.dump();
 
-    myConfig.add({"TRISB",0x000});
+    myConfig.add({"TRISB", 0x000});
     myConfig.dump();
-    std::println("Size = {0:d}",myConfig.getSize());
+    std::println("Size = {0:d}", myConfig.getSize());
 
     std::println("--- End Test ---");
-
 
     return 0;
 }
