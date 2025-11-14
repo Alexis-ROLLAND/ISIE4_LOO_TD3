a501c41489410c12c6ce04d61b062765c033c055
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-11-14 10:16:15 +0100

    Few improvements

--- src/Config.cpp
+++ src/Config.cpp
@@ -2,20 +2,20 @@
 
 //----------------------------------------------------------------------------
 void Config::add(const pic24_register &reg, bool ChangeIfExists) {
-    auto it = this->ConfigList.find(reg.first);
-    if (it == this->ConfigList.end()) {
+
+    if (this->ConfigList.contains(reg.first) == false) {
         this->ConfigList.insert(reg); /**< The key doesn't exist in the map, ok, insert the new one   */
         std::println(std::clog, "Register {0:s} has been successfully inserted.", reg.first); /**< Affichage */
     } else {
         if (ChangeIfExists) {
-            this->ConfigList.erase(it);   /**< First, erase the element */
-            this->ConfigList.insert(reg); /**< then add the new one */
+            this->ConfigList.at(reg.first) = reg.second; /**< then add the new one */
             std::println(std::clog, "Register {0:s} has been successfully changed.", reg.first); /**< Affichage */
         } else {
             throw std::invalid_argument("Register already exists."); /**< Throw Exception */
         }
     }
 }
+
 //----------------------------------------------------------------------------
 void Config::remove(const std::string &key) {
     auto nbErased = this->ConfigList.erase(key);
